37e9843e42712061ca624b9392124891daeadc84
ShimabukuroYuki <shima.nobishiro@gmail.com>
2018-10-14 02:21:30 +0900

    c

--- js/main.js
+++ js/main.js
@@ -41,10 +41,8 @@ $(function(){
         scrollHeight = $(document).height();
         scrollPosition = $(window).height() + $(window).scrollTop();
         if ( (scrollHeight - scrollPosition) / scrollHeight <= 0.01) {
-            //スクロールの位置が下部5%の範囲に来た場合
             $('.scroll').fadeOut();
         } else {
-            //それ以外のスクロールの位置の場合
             $('.scroll').fadeIn();
         }
     });
